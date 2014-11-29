79ec80cbab891f595ef271a0e19eac51be5473c5
Robin Hübner <profan@pagefault.se>
2014-11-29 21:41:13 +0100

    Moved error handling to separate file.

--- website/__init__.py
+++ website/__init__.py
@@ -15,18 +15,7 @@ db.drop_all()
 db.create_all()
 
 from website.routes import *
-
-@app.errorhandler(403)
-def not_authorized(path):
-    return render_template('status/403.html'), 403
-
-@app.errorhandler(404)
-def page_not_found(path):
-    return render_template('status/404.html'), 404
-
-@app.errorhandler(410)
-def resource_gone(path):
-    return render_template('status/410.html'), 410
+from website.errorhandler import *
 
 if __name__ == '__main__':
     app.run()
