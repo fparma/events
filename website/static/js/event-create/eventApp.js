c2935241aeb468336ffdef85319975b587628a5c
Cuel <alleee@gmail.com>
2014-12-03 17:36:09 +0100

    changed from spaces to tabs

--- website/static/js/event-create/eventApp.js
+++ website/static/js/event-create/eventApp.js
@@ -2,75 +2,75 @@
 
 angular.module('fpEvents', ['fpEvents.create', 'fpEvents.slots'])
 
-    .config(['$interpolateProvider', '$httpProvider',
+.config(['$interpolateProvider', '$httpProvider',
         function ($interpolateProvider, $httpProvider) {
-            $interpolateProvider.startSymbol('[[');
-            $interpolateProvider.endSymbol(']]');
-            delete $httpProvider.defaults.headers.common["X-Requested-With"];
-            
+		$interpolateProvider.startSymbol('[[');
+		$interpolateProvider.endSymbol(']]');
+		delete $httpProvider.defaults.headers.common["X-Requested-With"];
+
     }]).factory('FileUpload', ['$http', '$q', '$location',
         function ($http, $q, $location) {
 
-            function getFormHeaders() {
-                return {
-                    'Content-Type': undefined
-                };
-            }
+		function getFormHeaders() {
+			return {
+				'Content-Type': undefined
+			};
+		}
 
-            return {
-                getPlayableUnitsFromFile: function (file) {
-                    /*global FormData: false*/
+		return {
+			getPlayableUnitsFromFile: function (file) {
+				/*global FormData: false*/
 
-                    var dfd = $q.defer();
-                    var fd = new FormData();
-                    fd.append('sqmFile', file);
-                    $http.post('http://' + $location.host() + ':8080/api/parse-sqm-file', fd, {
-                        transformRequest: angular.identity,
-                        headers: getFormHeaders()
-                    }).success(function (response) {
-                        if (response.data) {
-                            dfd.resolve(response.data);
-                        } else {
-                            // failed to parse
-                            dfd.reject(response.error);
-                        }
-                    }).error(function (e) {
-                        // server is down?
-                        console.log(e);
-                    });
-                    return dfd.promise;
-                },
+				var dfd = $q.defer();
+				var fd = new FormData();
+				fd.append('sqmFile', file);
+				$http.post('http://' + $location.host() + ':8080/api/parse-sqm-file', fd, {
+					transformRequest: angular.identity,
+					headers: getFormHeaders()
+				}).success(function (response) {
+					if (response.data) {
+						dfd.resolve(response.data);
+					} else {
+						// failed to parse
+						dfd.reject(response.error);
+					}
+				}).error(function (e) {
+					// server is down?
+					console.log(e);
+				});
+				return dfd.promise;
+			},
 
-                uploadMissionImage: function (imageFile) {
-                    var fd = new FormData();
-                    fd.append('file', imageFile);
-                    return $http.post('/upload', fd, {
-                        transformRequest: angular.identity,
-                        headers: getFormHeaders()
-                    });
+			uploadMissionImage: function (imageFile) {
+				var fd = new FormData();
+				fd.append('file', imageFile);
+				return $http.post('/upload', fd, {
+					transformRequest: angular.identity,
+					headers: getFormHeaders()
+				});
 
-                }
-            };
+			}
+		};
 
     }]).service('EventService', ['$http', '$window', '$location',
         function ($http, $window, $location) {
-            // TODO should redirect really happen here
-            this.createNewEvent = function (data) {
-                return $http({
-                    url: '/create',
-                    method: 'POST',
-                    data: data,
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }).success(function (data) {
-                    console.log('success');
-                    console.log(data);
-                    //redirect
-                    $window.location.href = ('http://' + $location.host() + ':' + $location.port());
-                }).error(function (data) {
-                    console.log('error');
-                    console.log(data);
-                });
-            };  
+		// TODO should redirect really happen here
+		this.createNewEvent = function (data) {
+			return $http({
+				url: '/create',
+				method: 'POST',
+				data: data,
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}).success(function (data) {
+				console.log('success');
+				console.log(data);
+				//redirect
+				$window.location.href = ('http://' + $location.host() + ':' + $location.port());
+			}).error(function (data) {
+				console.log('error');
+				console.log(data);
+			});
+		};
 }]);
--- website/static/js/event-create/eventSlotsCtrl.js
+++ website/static/js/event-create/eventSlotsCtrl.js
@@ -1,131 +1,131 @@
 /*global angular: false*/
 
 angular.module('fpEvents.slots', [])
-    .controller('slotsCtrl', ['$scope', '$http', '$timeout', 'FileUpload', 'EventService',
+	.controller('slotsCtrl', ['$scope', '$http', '$timeout', 'FileUpload', 'EventService',
             function ($scope, $http, $timeout, FileUpload, EventService) {
 
-            function resetSides() {
-                $scope.sideSlots = {
-                    west: [],
-                    east: [],
-                    guer: [],
-                    civ: []
-                };
-            }
-            resetSides();
+			function resetSides() {
+				$scope.sideSlots = {
+					west: [],
+					east: [],
+					guer: [],
+					civ: []
+				};
+			}
+			resetSides();
 
-            // helper array to keep the map ordered
-            $scope.orderedSides = [
-                {
-                    getGroups: function () {
-                        return $scope.sideSlots.west;
-                    },
-                    displayName: 'Blufor',
-                    sideName: 'west'
+			// helper array to keep the map ordered
+			$scope.orderedSides = [
+				{
+					getGroups: function () {
+						return $scope.sideSlots.west;
+					},
+					displayName: 'Blufor',
+					sideName: 'west'
                 },
-                {
-                    getGroups: function () {
-                        return $scope.sideSlots.east;
-                    },
-                    displayName: 'Opfor',
-                    sideName: 'east'
+				{
+					getGroups: function () {
+						return $scope.sideSlots.east;
+					},
+					displayName: 'Opfor',
+					sideName: 'east'
                 },
-                {
-                    getGroups: function () {
-                        return $scope.sideSlots.guer;
-                    },
-                    displayName: 'Independent',
-                    sideName: 'guer'
+				{
+					getGroups: function () {
+						return $scope.sideSlots.guer;
+					},
+					displayName: 'Independent',
+					sideName: 'guer'
                 },
-                {
-                    getGroups: function () {
-                        return $scope.sideSlots.civ;
-                    },
-                    displayName: 'Civilian',
-                    sideName: 'civ'
+				{
+					getGroups: function () {
+						return $scope.sideSlots.civ;
+					},
+					displayName: 'Civilian',
+					sideName: 'civ'
                 },
             ];
 
-            $scope.editManually = function () {
-                resetSides();
-                $scope.addNewGroup('west');
-            };
+			$scope.editManually = function () {
+				resetSides();
+				$scope.addNewGroup('west');
+			};
 
-            $scope.updateSideSelection = function (e, side) {
-                var isAdd = e.target.checked;
-                if (isAdd) {
-                    $scope.addNewGroup(side);
-                } else {
-                    //remove
-                    $scope.sideSlots[side] = [];
-                }
-            };
+			$scope.updateSideSelection = function (e, side) {
+				var isAdd = e.target.checked;
+				if (isAdd) {
+					$scope.addNewGroup(side);
+				} else {
+					//remove
+					$scope.sideSlots[side] = [];
+				}
+			};
 
-            $scope.anySideHaveUnits = function () {
-                return ['west', 'east', 'guer', 'civ'].some($scope.sideHasUnits);
-            };
+			$scope.anySideHaveUnits = function () {
+				return ['west', 'east', 'guer', 'civ'].some($scope.sideHasUnits);
+			};
 
-            $scope.sideHasUnits = function (side) {
-                var slotSide = $scope.sideSlots[side];
-                for (var key in slotSide) {
-                    var units = slotSide[key].units;
-                    if (units && units.length > 0) {
-                        return true;
-                    }
-                }
-                return false;
-            };
+			$scope.sideHasUnits = function (side) {
+				var slotSide = $scope.sideSlots[side];
+				for (var key in slotSide) {
+					var units = slotSide[key].units;
+					if (units && units.length > 0) {
+						return true;
+					}
+				}
+				return false;
+			};
 
-            $scope.addNewGroup = function (side, name) {
-                $scope.sideSlots[side].push({
-                    name: name || 'Group',
-                    side: side,
-                    units: [{}, {}, {}, {}, {}, {}, {}, {}]
-                });
-            };
+			$scope.addNewGroup = function (side, name) {
+				$scope.sideSlots[side].push({
+					name: name || 'Group',
+					side: side,
+					units: [{}, {}, {}, {}, {}, {}, {}, {}]
+				});
+			};
 
-            $scope.deleteGroup = function (side, idx) {
-                $scope.sideSlots[side].splice(idx, 1);
-            };
+			$scope.deleteGroup = function (side, idx) {
+				$scope.sideSlots[side].splice(idx, 1);
+			};
 
-            $scope.addUnitToGroup = function (group) {
-                return group.units.push({});
-            };
+			$scope.addUnitToGroup = function (group) {
+				return group.units.push({});
+			};
 
-            $scope.removeUnitFromGroup = function (group) {
-                return group.units.splice(group.length - 1, 1);
-            };
+			$scope.removeUnitFromGroup = function (group) {
+				return group.units.splice(group.length - 1, 1);
+			};
 
-            $scope.uploadSQMFile = function () {
+			$scope.uploadSQMFile = function () {
 
-                var file = $scope.sqmFile;
-                FileUpload.getPlayableUnitsFromFile(file)
-                    .then(function ok(data) {
-                        $timeout(function () {
-                            $scope.sideSlots = data;
-                        }, 0);
-                    }, function error(e) {
-                        console.log(e);
-                    });
-            };
+				var file = $scope.sqmFile;
+				FileUpload.getPlayableUnitsFromFile(file)
+					.then(function ok(data) {
+						$timeout(function () {
+							$scope.sideSlots = data;
+						}, 0);
+					}, function error(e) {
+						console.log(e);
+					});
+			};
 
-            $scope.checkValidAndSubmit = function () {
-                var slots = {};
-                for (var i in $scope.sideSlots) {
-                    if ($scope.sideSlots[i].length) {
-                        slots[i] = $scope.sideSlots[i];
-                    }
-                }
-                var data = angular.toJson({
-                    eventType: $scope.eventType.name,
-                    eventImageUrl: $scope.eventImageUrl,
-                    eventSlotsNumber: parseInt($scope.eventSlots, 10),
-                    eventName: $scope.eventName,
-                    eventNameFull: $scope.eventType.name + $scope.eventSlots + ' - ' + $scope.eventName,
-                    eventDescription: $scope.eventDescription,
-                    eventSlots: slots
-                });
+			$scope.checkValidAndSubmit = function () {
+				var slots = {};
+				for (var i in $scope.sideSlots) {
+					if ($scope.sideSlots[i].length) {
+						slots[i] = $scope.sideSlots[i];
+					}
+				}
+				var data = angular.toJson({
+					eventType: $scope.eventType.name,
+					eventImageUrl: $scope.eventImageUrl,
+					eventSlotsNumber: parseInt($scope.eventSlots, 10),
+					eventName: $scope.eventName,
+					eventNameFull: $scope.eventType.name + $scope.eventSlots + ' - ' + $scope.eventName,
+					eventDescription: $scope.eventDescription,
+					eventSlots: slots
+				});
 
-                EventService.createNewEvent(data);
-            };
+				EventService.createNewEvent(data);
+			};
 }]);
